187ca065154b1e2587561c39e5bed20bbbd7a4f6
billyeatcookies <billydevbusiness@gmail.com>
2021-03-17 23:43:03 +0530

    Added PasteBotNet

---
+++ PasteBotNet/PasteBotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PasteBotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
