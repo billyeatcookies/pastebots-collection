9e8e65a178b476ae8cf37ba316700eedeaf24206
billyeatcookies <billydevbusiness@gmail.com>
2021-03-21 15:35:13 +0530

    PasteBot model added

--- PasteBot.py/cogs/events/paste.py
+++ PasteBot.py/cogs/events/paste.py
@@ -5,7 +5,7 @@ import pastemyst
 from discord.ext import commands
 
 regex = "(\`{3}[a-z]*\n[\s\S]*?\n\`{3})"
-client = pastemyst.Client(key='', is_dev=True)
+client = pastemyst.Client(key='')
 
 
 def is_code_block(msg):
@@ -26,23 +26,23 @@ class Paste(commands.Cog):
             description=f"Code block by {msg.author.mention} was pasted on pastemyst!" +
                         f"\n[Click here to view it!]({link})",
             color=discord.Color.green()
-        ).set_author(name="Pasted!", url=link, icon_url=msg.author.icon_url)
+        ).set_author(name="Pasted!", url=link, icon_url=msg.author.avatar_url)
         await msg.channel.send(embed=embed)
         await msg.delete()
 
     @staticmethod
     async def paste_message(msg):
-        language = ""
+        # language = ""
         code = ""
 
         matches = re.findall(regex, msg)
 
         for match in matches:
             lines = re.split("\n", match)
-            lines[0] = lines[0].lstrip("`")
-
-            if len(lines[0]) > 0:
-                language = lines[0]
+            # lines[0] = lines[0].lstrip("`")
+            #
+            # if len(lines[0]) > 0:
+            #     language = lines[0]
             lines.pop(0)
             lines.pop(len(lines) - 1)
             code = "\n".join(lines)
@@ -52,10 +52,10 @@ class Paste(commands.Cog):
 
         for match in matches:
             pasty = pastemyst.Pasty(code=code)
-            # pasty.language = pastemyst.Language.AUTODETECT
-            if not language.isspace() or language.strip():
-                pastemyst_language = client.get_language_info(ext=language)
-                pasty.language = pastemyst_language.name | pastemyst.Language.AUTODETECT
+            pasty.language = pastemyst.Language.AUTODETECT
+            # if not language.isspace() or language.strip():
+            #     pastemyst_language = client.get_language_info(ext=language)
+            #     pasty.language = pastemyst_language.name
             pasties.append(pasty)
 
         paste.pasties = pasties
