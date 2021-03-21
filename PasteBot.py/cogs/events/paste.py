18a58449ee4f2cf5875ab3594879de39ba1a5c47
billyeatcookies <billydevbusiness@gmail.com>
2021-03-21 13:56:05 +0530

    Initialize project

---
+++ PasteBot.py/cogs/events/paste.py
@@ -0,0 +1,68 @@
+import re
+
+import discord
+import pastemyst
+from discord.ext import commands
+
+regex = "(\`{3}[a-z]*\n[\s\S]*?\n\`{3})"
+client = pastemyst.Client(key='', is_dev=True)
+
+
+def is_code_block(msg):
+    return re.findall(regex, msg) != []
+
+
+class Paste(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        if (msg.author.id == self.bot.user.id) or not (is_code_block(msg.content)):
+            return
+        # msg_wrapped_line_count = re.split('\n', msg.Content)[]
+        link = await self.paste_message(msg.content)
+        embed = discord.Embed(
+            description=f"Code block by {msg.author.mention} was pasted on pastemyst!" +
+                        f"\n[Click here to view it!]({link})",
+            color=discord.Color.green()
+        ).set_author(name="Pasted!", url=link, icon_url=msg.author.icon_url)
+        await msg.channel.send(embed=embed)
+        await msg.delete()
+
+    @staticmethod
+    async def paste_message(msg):
+        language = ""
+        code = ""
+
+        matches = re.findall(regex, msg)
+
+        for match in matches:
+            lines = re.split("\n", match)
+            lines[0] = lines[0].lstrip("`")
+
+            if len(lines[0]) > 0:
+                language = lines[0]
+            lines.pop(0)
+            lines.pop(len(lines) - 1)
+            code = "\n".join(lines)
+
+        paste = pastemyst.Paste(expires_in=pastemyst.ExpiresIn.ONE_HOUR)
+        pasties = []
+
+        for match in matches:
+            pasty = pastemyst.Pasty(code=code)
+            # pasty.language = pastemyst.Language.AUTODETECT
+            if not language.isspace() or language.strip():
+                pastemyst_language = client.get_language_info(ext=language)
+                pasty.language = pastemyst_language.name | pastemyst.Language.AUTODETECT
+            pasties.append(pasty)
+
+        paste.pasties = pasties
+        result = client.create_paste(paste=paste)
+
+        return f"https://paste.myst.rs/{result._id}"
+
+
+def setup(bot):
+    bot.add_cog(Paste(bot))
--- PasteBot.py/data/Extensions.py
+++ PasteBot.py/data/Extensions.py
@@ -6,39 +6,29 @@ import json
 
 class Extensions:
     def __init__(self):
-        self.main_cog = "main"
         self.events_cog = "events"
-        self.game_cog = "game"
+        self.paste_cog = "paste"
         self.load_extensions()
 
     def load_extensions(self):
-        self.load_main_cog()
         self.load_events_cog()
-        self.load_game_cog()
-
-    def load_main_cog(self):
-        with open("data/extensions.json") as file:
-            data = json.load(file)
-            self.main_cog = data[self.main_cog]
+        self.load_paste_cog()
 
     def load_events_cog(self):
         with open("data/extensions.json") as file:
             data = json.load(file)
             self.events_cog = data[self.events_cog]
 
-    def load_game_cog(self):
+    def load_paste_cog(self):
         with open("data/extensions.json") as file:
             data = json.load(file)
-            self.game_cog = data[self.game_cog]
-
-    def get_main_cog(self):
-        return self.main_cog
+            self.paste_cog = data[self.paste_cog]
 
     def get_events_cog(self):
         return self.events_cog
 
-    def get_game_cog(self):
-        return self.game_cog
+    def get_paste_cog(self):
+        return self.paste_cog
 
     def get_all_extensions(self):
-        return [self.get_main_cog(), self.get_events_cog(), self.get_game_cog()]
+        return [self.get_events_cog(), self.get_paste_cog()]
